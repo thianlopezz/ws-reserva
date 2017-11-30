f2ba6aad942b3bbd4eab550eb80229cb0424302a
thianlopezz <thianlopezz@gmail.com>
2017-11-30 13:33:34 -0500

    subida pregunta

--- Models/chocolatada.js
+++ Models/chocolatada.js
@@ -2,16 +2,21 @@ var connection = require('../connection');
 
 function chocolatada() {
 
-    this.setPregunta = function (idpregunta, criterio, res) {
+    this.setPregunta = function (idpregunta, res) {
         connection.acquire(function (err, con) {
-            con.query('call recibe_respuesta(\'' + idpregunta + '\', \'' + criterio + '\')', function (err, result) {
+            con.query("call recibe_respuesta('" + idpregunta + "' )", function (err, result) {
                 con.release();
 
                 if (err){
+                    console.log(err);
                     res.send({ success: false, mensaje: 'Lo siento algo salió mal!' });
+                } else {
+                    if (result[0][0].success === 1){
+                        res.send({ success: true, mensaje: result[0][0].mensaje });
+                    } else {
+                        res.send({ success: false, mensaje: result[0][0].mensaje });
+                    }
                 }                    
-                else
-                    res.send(result[0][0]);
             });
         });
     };
